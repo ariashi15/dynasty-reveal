326c1c094747eec16a1e005a3d62ef06317d68be
Aria Shi <ariashi15@gmail.com>
2026-04-21 12:17:36 -0500

    changed pathname

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const DYNASTIES_ROOT_PATH = '/dynasties'
+const normalizedPath = window.location.pathname.replace(/\/+$/, '') || '/'
+
+if (normalizedPath === '/') {
+  window.history.replaceState(null, '', `${DYNASTIES_ROOT_PATH}${window.location.search}${window.location.hash}`)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
